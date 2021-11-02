f586b7fb243b5b413f59716885bc429e82560089
ngdloc2000 <ngdinhloc2000@gmail.com>
2021-11-02 14:28:17 +0700

    nop bai

--- quanlyvattu/src/taostored.sql
+++ quanlyvattu/src/taostored.sql
@@ -3,41 +3,48 @@ use quanlyvattu;
 -- 1. Tạo Stored procedure (SP) cho biết tổng số lượng cuối của vật tư
 -- với mã vật tư là tham số vào.
 delimiter //
-create procedure sp_GetAmount(IN vtId int)
+create procedure sp_GetAmount(IN vtId int, OUT total int)
 begin
-    select v.vtName, (tk.tkSoLuongDau + tk.tkSoLuongNhap - tk.tkSoLuongXuat) as 'Số lượng cuối'
+    select (tk.tkSoLuongDau + tk.tkSoLuongNhap - tk.tkSoLuongXuat) as 'Số lượng cuối'
+    into total
     from tonkho tk
              join vattu v on v.vtId = tk.vtId
     where v.vtId = vtId;
 end //
 delimiter ;
-call sp_GetAmount(1);
+
+call sp_GetAmount(1, @total);
+select @total;
 
 -- 2. Tạo SP cho biết tổng tiền xuất của vật tư với mã vật tư là tham số vào.
 delimiter //
-create procedure sp_TongTienXuat(IN vtId int)
+create procedure sp_TongTienXuat(IN vtId int, OUT total bigint)
 begin
-    select v.vtName, sum(c.donGiaXuat * c.soLuongXuat) as 'Tổng tiền xuất'
+    select sum(c.donGiaXuat * c.soLuongXuat) as 'Tổng tiền xuất'
+    into total
     from chitietphieuxuat c
              join vattu v on c.vtId = v.vtId
     group by v.vtId
     having v.vtId = vtId;
 end //
 delimiter ;
-call sp_TongTienXuat(3);
+call sp_TongTienXuat(3, @total);
+select @total;
 
 -- 3. Tạo SP cho biết tổng số lượng đặt theo số đơn hàng với số đơn hàng là tham số vào.
 delimiter //
-create procedure sp_TongSLDat(IN ddhId int)
+create procedure sp_TongSLDat(IN ddhId int, OUT total int)
 begin
-    select d.ddhCode, sum(c.soLuongDat) as 'Số lượng đặt'
+    select sum(c.soLuongDat) as 'Số lượng đặt'
+    into total
     from chitietdonhang c
              join dondathang d on d.ddhId = c.ddhId
     group by d.ddhId
     having d.ddhId = ddhId;
 end //
-
-call sp_TongSLDat(1);
+delimiter ;
+call sp_TongSLDat(1, @total);
+select @total;
 
 -- 4. Tạo SP dùng để thêm một đơn đặt hàng
 delimiter //
